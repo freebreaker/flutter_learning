99ee9a7776da2d74892de9de43dc751aa6751857
freebreaker <592378845@qq.com>
2019-07-25 22:27:49 +0800

    725 evening

--- lib/page/frontPage/card_everyday.dart
+++ lib/page/frontPage/card_everyday.dart
@@ -1,18 +1,48 @@
 import "package:flutter/material.dart";
 
 class CardEveryday extends StatefulWidget {
-  CardEveryday({Key key,this.title,this.author}):super(key:key);
+  CardEveryday({Key key, this.content, this.author}) : super(key: key);
 
-  final String title;
+  final String content;
   final String author;
 
-  @override 
-  _CardEveryday createState()=>_CardEveryday();
+  @override
+  _CardEveryday createState() => _CardEveryday();
 }
 
-class _CardEveryday extends State<CardEveryday>{
-  @override 
-  Widget build(BuildContext context){
-    return Text("123");
+class _CardEveryday extends State<CardEveryday> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Card(
+        margin: EdgeInsets.all(18),
+        // color: Colors.redAccent[200],
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            ListTile(
+              leading: Icon(Icons.date_range),
+              title: Text("2019/7/24"),
+              // subtitle: Text('Music by Julie Gable. Lyrics by Sidney Stein.'),
+            ),
+            ButtonTheme.bar(
+              // make buttons use the appropriate styles for cards
+              child: ButtonBar(
+                children: <Widget>[
+                  FlatButton(
+                    child: Text('BUY TICKETS'),
+                    onPressed: () {/* ... */},
+                  ),
+                  FlatButton(
+                    child: Text('LISTEN'),
+                    onPressed: () {/* ... */},
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/page/frontPage/front_page.dart
+++ lib/page/frontPage/front_page.dart
@@ -1,5 +1,7 @@
 import "package:flutter/material.dart";
-import 'package:myflutterapp/page/frontPage/front_page_list.dart';
+import 'package:myflutterapp/page/frontPage/card_everyday.dart';
+
+// import 'package:myflutterapp/page/frontPage/front_page_list.dart';
 
 class FrontPage extends StatefulWidget {
   @override
@@ -27,53 +29,36 @@ class _FrontPage extends State<FrontPage> with SingleTickerProviderStateMixin {
     label: Text("提问", style: TextStyle(color: Colors.blue, fontSize: 18.0)),
   );
 
-  Widget searchBar() {
-    return Row(
-      children: <Widget>[
-        Expanded(
-          child: Padding(
-            padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
-            child: FlatButton.icon(
-              color: Colors.black12,
-              onPressed: () {},
-              icon: Icon(Icons.search, size: 18.0, color: Colors.grey),
-              label: Text(
-                "搜索",
-                style: TextStyle(color: Colors.grey, fontSize: 16.0),
-              ),
-            ),
-          ),
-        )
-      ],
-    );
-  }
+  // Widget searchBar() {
+  //   return Row(
+  //     children: <Widget>[
+  //       Expanded(
+  //         child: Padding(
+  //           padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+  //           child: FlatButton.icon(
+  //             color: Colors.black12,
+  //             onPressed: () {},
+  //             icon: Icon(Icons.search, size: 18.0, color: Colors.grey),
+  //             label: Text(
+  //               "搜索",
+  //               style: TextStyle(color: Colors.grey, fontSize: 16.0),
+  //             ),
+  //           ),
+  //         ),
+  //       )
+  //     ],
+  //   );
+  // }
 
   @override
   Widget build(BuildContext context) {
-    return new DefaultTabController(
-        length: 4,
-        child: new Scaffold(
-            appBar: new AppBar(
-              title: searchBar(),
-              elevation: 0,
-              bottom: new TabBar(
-                tabs: <Widget>[
-                  new Tab(text: "关注"),
-                  new Tab(text: "推荐"),
-                  new Tab(text: "热榜"),
-                  new Tab(text: "视频"),
-                ],
-                controller: _tabController,
-              ),
-            ),
-            body: TabBarView(
-              controller: _tabController,
-              children: <Widget>[
-                FrontPageList(),
-                new Center(child: new Text('船')),
-                new Center(child: new Text('巴士')),
-                new Center(child: new Text('火车')),
-              ],
-            )));
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Hi"),
+        elevation: 0,
+      ),
+      backgroundColor: Colors.grey[200],
+      body: CardEveryday(content: "白日依山尽，黄河入海流",author: "free"),
+    );
   }
 }
